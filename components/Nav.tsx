7affe7c3df5f5c2df4abe87ea06bac808456b30e
RealRiga <116930426+RealRiga@users.noreply.github.com>
2024-02-01 05:13:03 +0100

    changes

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,10 +8,10 @@ const Nav = ({ openNav }: Props) => {
     return (
         <div className='w-[100%] fixed z-[10000] top-0 h-[12vh] bg-[#141c27] shadow-md'>
             <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
-                {/* <h1 className='flex-[0.6] cursor-pointer text-[25px] text-white font-bold'>
+                <h1 className='flex-[0.6] cursor-pointer text-[25px] text-white font-bold'>
                     Ri
                     <span className='text-yellow-300'>ga</span>
-                </h1> */}
+                </h1>
                 <div className='nav-link'>Home</div>
                 <div className='nav-link'>Services</div>
                 <div className='nav-link'>About</div>
